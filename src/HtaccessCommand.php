6220df9509232a38a74ab4b8a41a71e9cb1edc16
woutersioen <wouter.sioen@gmail.com>
2019-11-20 13:37:59 +0100

    Add an Htaccess command
    I made this the default command to make sure that you can just run
    `htaccess` without having to specify the `run` command.

---
+++ src/HtaccessCommand.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Madewithlove;
+
+use Symfony\Component\Console\Command\Command;
+
+final class HtaccessCommand extends Command
+{
+    protected static $defaultName = 'run';
+}

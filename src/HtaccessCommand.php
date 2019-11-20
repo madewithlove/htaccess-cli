5fc7497b8f81f9c87babb09a9476804b2ab9b744
woutersioen <wouter.sioen@gmail.com>
2019-11-20 13:39:10 +0100

    Add a dummy execute method
    Every command needs to have an execute method, this is where the magic
    happens.

--- src/HtaccessCommand.php
+++ src/HtaccessCommand.php
@@ -3,8 +3,15 @@
 namespace Madewithlove;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 final class HtaccessCommand extends Command
 {
     protected static $defaultName = 'run';
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Hello!');
+    }
 }

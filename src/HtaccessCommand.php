8937c8e4c547b00ac9584f71f806de2b1c04fab3
woutersioen <wouter.sioen@gmail.com>
2019-11-20 13:44:29 +0100

    Add a url as a required argument
    It looks like this only works when you pass a command name to the binary
    (so with `htaccess run` instead of just `htaccess`) so we'll need to
    change that setup there a bit.

--- src/HtaccessCommand.php
+++ src/HtaccessCommand.php
@@ -3,12 +3,18 @@
 namespace Madewithlove;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 final class HtaccessCommand extends Command
 {
     protected static $defaultName = 'run';
+
+    protected function configure()
+    {
+        $this->addArgument('url', InputArgument::REQUIRED, 'The request url to test your .htaccess file with');
+    }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {

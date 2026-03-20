69509eaebea551591d2d66a4e050a9807b7fdde8
jdrieghe <jonasd@madewithlove.com>
2026-03-20 09:26:17 +0100

    Replace $defaultName with #[AsCommand] attribute
    Fixes PHPStan errors: untyped static property and mixed passed to
    parent::__construct(). The attribute is the idiomatic Symfony approach
    since Console 5.3.

--- src/HtaccessCommand.php
+++ src/HtaccessCommand.php
@@ -9,6 +9,7 @@ use Madewithlove\HtaccessApiClient\HtaccessException;
 use Madewithlove\HtaccessApiClient\HtaccessResult;
 use Madewithlove\HtaccessApiClient\ServerVariables;
 use RuntimeException;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\RuntimeException as SymfonyRuntimeException;
 use Symfony\Component\Console\Input\InputArgument;
@@ -18,15 +19,14 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Yaml\Yaml;
 
+#[AsCommand(name: 'htaccess')]
 final class HtaccessCommand extends Command
 {
-    protected static $defaultName = 'htaccess';
-
     public function __construct(
         private HtaccessClient $htaccessClient,
         private TableRenderer $tableRenderer
     ) {
-        parent::__construct(self::$defaultName);
+        parent::__construct();
     }
 
     protected function configure(): void
